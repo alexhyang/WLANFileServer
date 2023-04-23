4fff5b4242f7ea94abdb5bca8d7fb0ea87bce365
Alex Yang <alex.hyang.dev@gmail.com>
2023-04-23 00:36:33 -0700

    improve utils by adding ext filter

--- utils/sendJSONList.js
+++ utils/sendJSONList.js
@@ -1,9 +1,25 @@
+const path = require("path");
 const fs = require("fs");
 const ip = require("ip");
 
+var getFileType = function (file) {
+  var images = [".jpg", ".jpeg", ".png", ".gif", ".webp", ".tiff"];
+  var videos = [".mp4", ".rmvb", ".mkv", ".wmv"];
+  var texts = [".txt"];
+
+  var ext = path.extname(file);
+  test = ext in images;
+  if (images.includes(ext)) return "images";
+  if (videos.includes(ext)) return "videos";
+  if (texts.includes(ext)) return "stories";
+};
+
+var isSameTypeOfFile = function (file, folder) {
+  return getFileType(file) === folder;
+};
+
 var sendJSONList = function (key, res, folder) {
   var dir = `./assets/${folder}/${key}`;
-  console.log(dir);
   fs.readdir(dir, (err, files) => {
     if (err) {
       console.log(err);
@@ -11,9 +27,13 @@ var sendJSONList = function (key, res, folder) {
     } else {
       const urls = [];
       files.forEach((file) => {
-        urls.push(
-          `http://${ip.address()}:${process.env.PORT}/${folder}/${key}/${file}`
-        );
+        if (isSameTypeOfFile(file, folder)) {
+          urls.push(
+            `http://${ip.address()}:${
+              process.env.PORT
+            }/${folder}/${key}/${file}`
+          );
+        }
       });
       res.json(urls);
     }

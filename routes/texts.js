a31ba7b7a1ea3c0be32be4b6ae63f3f8c4ce6ad5
Alex Yang <alex.hyang.dev@gmail.com>
2023-04-26 03:41:21 -0700

    add texts route

--- routes/texts.js
+++ routes/texts.js
@@ -1,12 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const path = require("path");
-const sendJSONList = require("../utils/sendJSONList");
 const getFileContent = require("../utils/getFileContent");
-
-router.get("/", (req, res) => {
-  sendJSONList(req.query.key, res, "stories");
-});
 
 router.get("/:filename(*)", (req, res) => {
   var content = getFileContent(req.params.filename);

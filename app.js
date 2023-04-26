a18c85a06397ea9d1572ed1a944caaff22e85ddb
Alex Yang <alex.hyang.dev@gmail.com>
2023-04-26 03:48:01 -0700

    update routers

--- app.js
+++ app.js
@@ -6,9 +6,10 @@ const logger = require("morgan");
 const cors = require("cors");
 
 const indexRouter = require("./routes/index");
+const searchRouter = require("./routes/search");
 const imageRouter = require("./routes/images");
 const videoRouter = require("./routes/videos");
-const storyRouter = require("./routes/stories");
+const textRouter = require("./routes/texts");
 
 dotenv.config();
 const app = express();
@@ -17,9 +18,10 @@ app.use(cors());
 app.use(logger("dev"));
 
 app.use("/", indexRouter);
+app.use("/search", searchRouter);
 app.use("/images", imageRouter);
 app.use("/videos", videoRouter);
-app.use("/stories", storyRouter);
+app.use("/stories", textRouter);
 
 app.listen(process.env.PORT, () => {
   console.log(`Listening on port ${process.env.PORT}!`);

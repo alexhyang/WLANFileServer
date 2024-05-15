989c7d22c47333c6c9823d19938fd79a1fd40b23
Alex Yang <alex.hyang.dev@gmail.com>
2024-05-15 00:37:47 -0700

    rename routers

--- app.js
+++ app.js
@@ -6,10 +6,10 @@ const logger = require("morgan");
 const cors = require("cors");
 
 const indexRouter = require("./routes/index");
-const searchRouter = require("./routes/search");
+const searchRouter = require("./routes/searchRouter");
 const imageRouter = require("./routes/images");
 const videoRouter = require("./routes/videos");
-const textRouter = require("./routes/texts");
+const textRouter = require("./routes/textRouter");
 const favRouter = require("./routes/favs");
 
 dotenv.config();
